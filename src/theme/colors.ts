a69eba37fb8a94240907167fd9ab70a549ee32cd
Wojciech Boman <wojciechboman@wojciechs-macbook-air.home>
2023-08-29 15:18:21 +0200

    Add Home screen

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+import palette from './palette';
+
+export const Colors = {
+  primary: palette.LIGHT_BLUE,
+  background: palette.WHITE,
+  primaryText: palette.BLACK,
+};
---
+++ src/theme/spacing.ts
@@ -0,0 +1,1 @@
+export const spacing = [0, 4, 8, 16, 32, 64, 128];
