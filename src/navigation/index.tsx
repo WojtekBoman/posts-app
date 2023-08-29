23958e8bfbcb1c06e470ed1d087285538b6593a4
Wojciech Boman <wojciechboman@wojciechs-macbook-air.home>
2023-08-29 14:25:03 +0200

    Add initial config for redux and react-navigation, add babel-eslint, fix eslint config

---
+++ src/navigation/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
